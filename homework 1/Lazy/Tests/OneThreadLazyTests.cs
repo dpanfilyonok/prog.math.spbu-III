7ee94932e63c6ad48e0c09d8093ff428991d7153
Dmitriy <dmitriy.panfilyonok@gmail.com>
2018-11-03 21:54:00 +0300

    Test for single thread version added

--- homework 1/Lazy/Tests/OneThreadLazyTests.cs
+++ homework 1/Lazy/Tests/OneThreadLazyTests.cs
@@ -24,5 +24,31 @@ namespace Tests
         {
             var lazy = LazyFactory<string>.CreateOneThreadLazy(null);
         }
+
+        [TestMethod]
+        public void SupplierReturningNullShouldWorkCorrectly()
+        {
+            var lazy = LazyFactory<int?>.CreateOneThreadLazy(() => null);
+            var obj1 = lazy.Get();
+            var obj2 = lazy.Get();
+            Assert.IsNull(obj1);
+            Assert.IsNull(obj2);
+        }
+
+        [TestMethod]
+        public void SupplierShouldBeCalculatedOnce()
+        {
+            int counter = 0;
+            var supplier = new Func<int>(() => {
+                counter++;
+                return counter;
+            });
+
+            var lazy = LazyFactory<int>.CreateOneThreadLazy(supplier);
+            var obj1 = lazy.Get();
+            var obj2 = lazy.Get();
+            Assert.AreEqual(1, obj2);
+            Assert.AreEqual(1, counter);
+        }
     }
 }
