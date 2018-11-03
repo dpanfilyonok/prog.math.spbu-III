775dc50eb796f6f344700d1da974c201e0877788
Dmitriy <dmitriy.panfilyonok@gmail.com>
2018-11-03 20:47:14 +0300

    server work

--- homework 3/SimpleFTP/Source/SimpleFTPServer.cs
+++ homework 3/SimpleFTP/Source/SimpleFTPServer.cs
@@ -5,11 +5,17 @@ namespace Source
     using System.Net;
     using System.Net.Sockets;
     using System.Threading.Tasks;
+    using System.Collections.Generic;
 
     public class SimpleFTPServer
     {
         private TcpListener _tcpServer;
         private const int _port = 2121;
+        enum Methods
+        {
+            List = 1,
+            Get = 2
+        }
 
         public SimpleFTPServer()
         {
@@ -35,9 +41,53 @@ namespace Source
                 Console.WriteLine("Подключен клиент. Выполнение запроса...");
                 var networkStream = user.GetStream();
                 var reader = new StreamReader(networkStream);
-                var data = await reader.ReadToEndAsync();
+                var request = await reader.ReadToEndAsync();
+                var (method, path) = ParseRequest(request);
+                switch (method)
+                {
+                    case (int)Methods.List:
+                    // формируем ответ из контента:
+                        var content = GetListOfContentsInDir(path);
+                        break;
+                    case (int)Methods.Get:
+                        break;
+                    default:
+                        throw new WebException("Invalid method");
+                }
+                
                 // тут нужно просто асинхроно дать ответ
             });
+        }
+
+        private List<Tuple<string, bool>> GetListOfContentsInDir(string pathToDir)
+        {
+            if (!Directory.Exists(pathToDir))
+            {
+                throw new DirectoryNotFoundException(pathToDir);
+            }
+
+            var root = new DirectoryInfo(pathToDir);
+            var files = root.EnumerateFiles();
+            var dirs = root.EnumerateDirectories();
+            var listOfContent = new List<Tuple<string, bool>>();
+            
+            foreach (var file in files)
+            {
+                listOfContent.Add(new Tuple<string, bool>(file.Name, false));
+            }
+
+            foreach (var dir in dirs)
+            {
+                listOfContent.Add(new Tuple<string, bool>(dir.Name, true));
+            }
+
+            return listOfContent;
+        }
+
+        private (int method, string path) ParseRequest(string request)
+        {
+            var splited = request.Split();
+            return (Convert.ToInt32(splited[0]), splited[1]);
         }
     }
 }
