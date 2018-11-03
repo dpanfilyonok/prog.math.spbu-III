eb09f42234f43f4dec1144e6b6efe75d0f127768
Dmitriy <dmitriy.panfilyonok@gmail.com>
2018-11-03 21:16:30 +0300

    Comments added

--- homework 1/Lazy/Source/LazyFactory.cs
+++ homework 1/Lazy/Source/LazyFactory.cs
@@ -2,8 +2,16 @@ using System;
 
 namespace Source
 {
+    /// <summary>
+    /// Фабрика, обеспечивающая создание ленивых объектов в однопоточном и многопоточном режиме
+    /// </summary>
+    /// <typeparam name="T">Тип возвращаемого объекта</typeparam>
     public static class LazyFactory<T>
     {
+        /// <summary>
+        /// Создает ленивый объект на основе вычисления с гарантией корректной работы в однопоточном режиме
+        /// </summary>
+        /// <param name="supplier">Вычисление, предоставляющее объект</param>
         public static ILazy<T> CreateOneThreadLazy(Func<T> supplier)
         {
             if (supplier == null)
@@ -13,6 +21,11 @@ namespace Source
 
             return new OneThreadLazy<T>(supplier);
         }
+        
+        /// <summary>
+        /// Создает ленивый объект на основе вычисления с гарантией корректной работы в многопоточном режиме
+        /// </summary>
+        /// <param name="supplier">Вычисление, предоставляющее объект</param>
         public static ILazy<T> CreateMultiThreadLazy(Func<T> supplier)
         {
             if (supplier == null)
--- homework 1/Lazy/Source/MultiThreadLazy.cs
+++ homework 1/Lazy/Source/MultiThreadLazy.cs
@@ -2,13 +2,29 @@ using System;
 
 namespace Source
 {
+    /// <summary>
+    /// Класс, реализующий ленивое вычисление объекта многопоточно
+    /// </summary>
+    /// <typeparam name="T">Тип возвращаемого объекта</typeparam>
     internal class MultiThreadLazy<T> : ILazy<T>
     {
+        /// <summary>
+        /// Вычисление, предоставляющее объект
+        /// </summary>
         private Func<T> _supplier = null;
+        
         // volatile чтобы избежать ситуации, когда поле закэшировалось, 
         // но другие потоки ещё не знают об изменении в нем
+        /// <summary>
+        /// true, если объект создан, false иначе
+        /// </summary>
         private volatile bool _isObjectCreated = false;
+
+        /// <summary>
+        /// Ссылка на созданный объект
+        /// </summary>
         private T _createdObject;
+        
         private object _lockObject = new object();
 
         public MultiThreadLazy(Func<T> supplier)
