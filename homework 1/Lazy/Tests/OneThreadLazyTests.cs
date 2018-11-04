d89c7e4e0dfcd5b87d3291fcb5228190bc7096bc
Dmitriy Panfilyonok <31937213+dpanfilyonok@users.noreply.github.com>
2018-11-04 14:38:10 +0300

    Hw1 Lazy (#2)
    * first impl of lazy
    
    * Comments added
    
    * Test for single thread version added
    
    * Tests for multi thread lazy addded
    
    * Comments to test classes added
    
    * ci added
    
    * ci added v2
    
    * ci added v3
    
    * ci added v4
    
    * ci added v5
    
    * ci added v6

---
+++ homework 1/Lazy/Tests/OneThreadLazyTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Source;
+using System;
+
+namespace Tests
+{
+    /// <summary>
+    /// Тесты на корректность работы <see cref="OneThreadLazy">
+    /// </summary>
+    [TestClass]
+    public class OneThreadLazyTests
+    {
+        [TestMethod]
+        public void GetShouldReturnTheSameObject()
+        {
+            string testString = "hello";
+            Func<string> supplier = () => testString;
+
+            var lazy = LazyFactory<string>.CreateOneThreadLazy(supplier);
+            var obj1 = lazy.Get();
+            var obj2 = lazy.Get();
+
+            Assert.AreSame(obj1, obj2);
+        }
+
+        [TestMethod]
+        public void SupplierShouldBeCalculatedOnce()
+        {
+            int counter = 0;
+            var supplier = new Func<int>(() =>
+            {
+                counter++;
+                return counter;
+            });
+
+            var lazy = LazyFactory<int>.CreateOneThreadLazy(supplier);
+            var obj1 = lazy.Get();
+            var obj2 = lazy.Get();
+
+            Assert.AreEqual(1, counter);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullSupplierShouldThrowException()
+        {
+            var lazy = LazyFactory<string>.CreateOneThreadLazy(null);
+        }
+
+        [TestMethod]
+        public void SupplierReturningNullShouldWorkCorrectly()
+        {
+            var lazy = LazyFactory<int?>.CreateOneThreadLazy(() => null);
+            var obj1 = lazy.Get();
+            var obj2 = lazy.Get();
+
+            Assert.IsNull(obj1);
+            Assert.IsNull(obj2);
+        }
+    }
+}
